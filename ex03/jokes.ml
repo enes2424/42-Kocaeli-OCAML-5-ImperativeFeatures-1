2b6411bac623fbf410e876ed4fa2dfe50f294616
Enes Mahmut Ates <eates@k2m16s02.42kocaeli.com.tr>
2025-07-27 12:41:06 +0300

    ocaml

--- ex03/jokes.ml
+++ ex03/jokes.ml
@@ -1,16 +1,16 @@
 let read_jokes_from_file filename =
   let file = open_in filename in
-  let rec read_lines list =
-    try
-      let line = input_line file in
-      read_lines (line :: list)
-    with
-    | End_of_file -> 
-        close_in file;
-        List.rev list
-  in
-  let lines = read_lines [] in
-  Array.of_list lines
+  let lines = ref [] in
+  (try
+    while true do
+      lines := (input_line file) :: !lines
+    done
+  with
+  | End_of_file -> 
+      close_in file;
+      lines := List.rev !lines
+  );
+  Array.of_list !lines
 
 let () =
   let args = Sys.argv in
